125b468e76493f7222772fa4b5af2c417a92cb06
Jack Turner <s1928776@ed.ac.uk>
2023-02-26 14:57:24 +0000

    Generic demands calculated

--- tegola/original_work/proportion_testing.py
+++ tegola/original_work/proportion_testing.py
@@ -14,11 +14,28 @@ def read_input_file(filename):
             line = file.readline()
             entry = line.split(" ")
             entry = np.reshape(np.array([float(x) for x in entry[0:len(entry) - 1]]), (12,12))
+
+            entry = return_to_original(entry)
+            print(entry)
             list.append(entry)
     return list
 
+def return_to_original(matrix):
+    for i in range(12):
+        if i == 0 or i == 3:
+            matrix[i][10] = matrix[i][10] + matrix[i][11]
+            matrix[i][11] = 0
+
+            matrix[10][i] = matrix[10][i] + matrix[11][i]
+            matrix[11][i] = 0
+        else:
+            matrix[i][11] = matrix[i][10] + matrix[i][11]
+            matrix[i][10] = 0
+
+            matrix[11][i] = matrix[10][i] + matrix[11][i]
+            matrix[10][i] = 0
+    return matrix
 
 if __name__ == "__main__":
     filename = "thirty_seventy.txt"
     list = read_input_file(filename)
-    print(list)
