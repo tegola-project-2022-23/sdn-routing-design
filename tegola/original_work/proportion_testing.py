5d6e42ba4a3a1de2c6b9021c445b217203c81969
Jack Turner <s1928776@ed.ac.uk>
2023-02-27 10:28:58 +0000

    Completed analysis on optimum split percentage

--- tegola/original_work/proportion_testing.py
+++ tegola/original_work/proportion_testing.py
@@ -1,11 +1,11 @@
-from prelim_calculations import Calculate
-import pandas as pd
 import numpy as np
-import datetime
 import matplotlib.pyplot as plt
-import csv
 from demand_calculation import *
-import os
+from NetworkTopology import *
+from NetworkParser import *
+from TESolver import *
+from MIPSolver import *
+from helper import *
 
 def read_input_file(filename, proportion_to_mhi):
 
@@ -30,15 +30,35 @@ def perform_split(matrix, proportion_to_mhi):
         
     return split_improved(matrix, proportion_to_mhi)
 
-if __name__ == "__main__":
-    filename = "demands/max_demands.txt"
+def get_unmet_demands():
 
+    dict = {}
+
+    for index in range(101):
+        network_name = "b4"
+        network = parse_topology(network_name)
+        parse_demands(index, network)
+        parse_tunnels(network)
+        initialize_weights(network)
+
+        mip = CvxSolver()
+        solver = TESolver(mip, network)
+        solver.add_demand_constraints()
+        solver.add_edge_capacity_constraints()
+        solver.Maximize(get_max_flow_objective(network))
+
+        max_flow = solver.solve()
+        unmet_demands = sum(get_demands_unmet(network).values())
+        print(unmet_demands)
+        dict[index] = unmet_demands
+
+    return dict
+
+def setup_demand_files():
+    filename = "demands/max_demands.txt"
     name = ["demands" + str(i) for i in range(101)]
 
     for i in range(101):
-
-        list = []
-
         if i == 0:
             index = 0
         else:
@@ -50,6 +70,20 @@ if __name__ == "__main__":
                 for j in range(12):
                     file.write(str(entry[i][j]) + " ")
             file.write("\n")
+    return
+
+if __name__ == "__main__":
+    dict = get_unmet_demands()
+    keys = np.array(list(dict.keys()))
+    values = np.array(list(dict.values()))
+    fig, ax = plt.subplots(1, 1, figsize=(6,6))
+    ax.plot(keys[7:30], values[7:30])
+    ax.set_title("Unmet demands for varying proportions of traffic sent to MHI")
+    ax.set_xlabel("Traffic to MHI (%)")
+    ax.set_ylabel("Unmet Demands (Mb)")
+    plt.show()
+
+    # The region of 15-23% appears to be the best split for Tegola's demands
 
         
 
